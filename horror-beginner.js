362b644d6be2898644eecae0b6708bf94876f76b
ohagi123 <84771869+ohagi123@users.noreply.github.com>
2021-08-21 19:26:07 +0900

    Create horror-beginner.js

---
+++ horror-beginner.js
@@ -0,0 +1,31 @@
+let enter = document.getElementById('enter')
+let answer = document.getElementById('answer')
+let resultDivided = document.getElementById('result-area')
+let commentary = document.getElementById("commentary")
+
+//答え合わせが押された時の処理
+answer.onclick = function(){
+ const Enter = enter.value;
+ if (Enter === "") {
+     return;
+ }
+
+    if (Enter ==="普段吠えない犬が勢いよく父に向かって吠え出した") {
+        resultDivided.innerText = "正解です"
+        commentary.innerText = "解説？:祖母を殺した犯人は父親。父は昔から祖母に何かしらの恨みを持っていたのかもしれない"
+    }else if (Enter === "吠えた") {
+        resultDivided.innerText = "正解です"
+        commentary.innerText = "解説？:祖母を殺した犯人は父親。父は昔から祖母に何かしらの恨みを持っていたのかもしれない"
+    }else if (Enter === "父に向かって吠え出した") {
+        resultDivided.innerText = "正解です"
+        commentary.innerText = "解説？:祖母を殺した犯人は父親。父は昔から祖母に何かしらの恨みを持っていたのかもしれない"
+    }else if (Enter === "吠え出した") {
+        resultDivided.innerText = "正解です"
+        commentary.innerText = "解説？:祖母を殺した犯人は父親。父は昔から祖母に何かしらの恨みを持っていたのかもしれない"
+    }else{
+        resultDivided.innerText = "不正解です"
+        commentary.innerText = "ヒント:なぜ、父に向かって吠えだしたのだろう？"
+    }
+
+ 
+}
